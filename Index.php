2276bc9284837b97c7b4f9bacaaf3ea7377da0bc
Bas Lepelaar <6010805@mborijnland.nl>
2021-03-11 12:42:07 +0100

    nav fix

---
+++ Index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="language" content="NL">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Eindcase Flexbox Portfolio 2021">
+	<meta name="author" content="Bas Lepelaar">
+	<meta name="keywords" content="Eindcase, Flexbox, Portfolio">
+	
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	
+	<title>Eindcase Flexbox</title>
+	</head>
+<body>
+    <header>
+        <article id="logo">
+            <img src="images/logo.png" alt="">
+        </article>
+        <nav id="navigatie">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Producten</a></li>
+                <li><a href="#">Evenementen</a></li>
+                <li><a href="#">Aanbiedingen</a></li>
+                <li><a href="#">Over Ons</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="drie-blikjes">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </section>
+    </main>
+    <footer>
+        <article class="item-footer">
+            <p class="copyright">2021 EKKO</p>
+            <p><a href="#">Contact</a></p>
+            <p><a href="#">Privacy</a></p>
+            <p><a href="#">FAQ</a></p>
+        </article>
+    </footer>
+</body>
+</html>
